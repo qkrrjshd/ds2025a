88b9b9df502fe132fcd1fdb3252f87f2423f6d75
pakr geonhong <qkrrjsghd10@naver.comkr>
2025-03-17 19:38:04 +0900

    v0.3 0(lon n) Guess game

--- week02.py
+++ week02.py
@@ -1,3 +1,19 @@
-n = int(input("정수 입력: "))
-result = n * (n + 1) //2  # o(1)
-print(result)
+import random
+
+n = random.randrange(1,101)
+win = True
+for i in range(7,0,-1):
+    guess = int(input(f"{i}번 남았다 정신차려라! 정답을 다시처라: "))
+    if guess ==n:
+        win = True
+        break
+    else:
+        if guess > n:
+            print("정답보다 크다.")
+        else:
+            print("정답보다 작다.")
+        win = True
+if win:
+    print("정답이다 축하해!")
+else:
+    print(f"정답은 {n}이였다")
